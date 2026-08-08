20417b4eec1ab9f321a496b7b94f7661d3491f8c
Maria <aguidavsilva@gmail.com>
2026-08-08 20:03:34 -0300

    Adicionando GerenciadorMensagem com envio e remocao de mensagem

---
+++ src/main/java/br/edu/ufape/poo/UfapeMarket/negocio/cadastro/GerenciadorMensagem.java
@@ -0,0 +1,29 @@
+package br.edu.ufape.poo.UfapeMarket.negocio.cadastro;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.edu.ufape.poo.UfapeMarket.dados.InterfaceRepositorioMensagem;
+import br.edu.ufape.poo.UfapeMarket.negocio.basica.Mensagem;
+import br.edu.ufape.poo.UfapeMarket.negocio.excecoes.MensagemVaziaException;
+
+@Service
+public class GerenciadorMensagem {
+	
+	@Autowired
+	private InterfaceRepositorioMensagem colecaoMensagem;
+	
+	public Mensagem enviarMensagem(Mensagem entity)
+	throws MensagemVaziaException{
+		if(entity.getTexto() == null || entity.getTexto().trim().isEmpty()) {
+			throw new MensagemVaziaException();
+		}
+		return colecaoMensagem.save(entity);
+	}
+	
+	public void removerMensagemId(Long id) {
+		colecaoMensagem.deleteById(id);
+	}
+	
+}
