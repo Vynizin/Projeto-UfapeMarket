fab2ebfd4522444e13da97ca0f1f6f94de69d22b
Maria <aguidavsilva@gmail.com>
2026-08-08 10:00:20 -0300

    Adicionando CadastroCategoria com validacao de duplicidade

---
+++ src/main/java/br/edu/ufape/poo/UfapeMarket/negocio/cadastro/CadastroCategoria.java
@@ -0,0 +1,46 @@
+package br.edu.ufape.poo.UfapeMarket.negocio.cadastro;
+
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+import java.util.Optional;
+
+import br.edu.ufape.poo.UfapeMarket.dados.InterfaceColecaoCategoria;
+import br.edu.ufape.poo.UfapeMarket.negocio.basica.Categoria;
+import br.edu.ufape.poo.UfapeMarket.negocio.excecoes.CategoriaDuplicadaException;
+
+@Service
+public class CadastroCategoria {
+	@Autowired
+	private InterfaceColecaoCategoria colecaoCategoria;
+	
+	public Categoria salvarCategoria(Categoria entity) throws CategoriaDuplicadaException{
+		List<Categoria> categorias = colecaoCategoria.findAll();
+		
+		for(Categoria c : categorias) {
+			if(c.getNome() != null && c.getNome().equalsIgnoreCase(entity.getNome())) {
+				throw new CategoriaDuplicadaException(entity.getNome());
+			}
+		}
+		
+		
+		return colecaoCategoria.save(entity);
+		
+	}
+	
+	public List<Categoria> listarCategorias(){
+		return colecaoCategoria.findAll();
+	}
+	
+	public Optional<Categoria> encontrarCategoriaId(Long id){
+		return colecaoCategoria.findById(id);
+	}
+	
+	public void removerCategoriaId(Long id) {
+		colecaoCategoria.deleteById(id);
+	}
+	
+	public boolean verificarExistenciaCategoriaId(Long id) {
+		return colecaoCategoria.existsById(id);
+	}
+}
