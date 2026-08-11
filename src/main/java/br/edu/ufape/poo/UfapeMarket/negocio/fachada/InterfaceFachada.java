cab4aaa068a1debeb9e55f5afe90721a7290afcd
Maria <aguidavsilva@gmail.com>
2026-08-11 16:02:00 -0300

    Criando a interface da fachada

---
+++ src/main/java/br/edu/ufape/poo/UfapeMarket/negocio/fachada/InterfaceFachada.java
@@ -0,0 +1,56 @@
+package br.edu.ufape.poo.UfapeMarket.negocio.fachada;
+
+import java.util.List;
+
+import br.edu.ufape.poo.UfapeMarket.negocio.basica.Avaliacao;
+import br.edu.ufape.poo.UfapeMarket.negocio.basica.Categoria;
+import br.edu.ufape.poo.UfapeMarket.negocio.basica.Chat;
+import br.edu.ufape.poo.UfapeMarket.negocio.basica.Mensagem;
+import br.edu.ufape.poo.UfapeMarket.negocio.basica.Produto;
+import br.edu.ufape.poo.UfapeMarket.negocio.basica.Usuario;
+import br.edu.ufape.poo.UfapeMarket.negocio.basica.Venda;
+import br.edu.ufape.poo.UfapeMarket.negocio.excecoes.CategoriaDuplicadaException;
+import br.edu.ufape.poo.UfapeMarket.negocio.excecoes.MensagemVaziaException;
+import br.edu.ufape.poo.UfapeMarket.negocio.excecoes.ProdutoCategoriaObrigatoriaException;
+import br.edu.ufape.poo.UfapeMarket.negocio.excecoes.ProdutoEstoqueInsuficienteException;
+import br.edu.ufape.poo.UfapeMarket.negocio.excecoes.ProdutoQuantidadeInvalidaException;
+import br.edu.ufape.poo.UfapeMarket.negocio.excecoes.UsuarioEmailInvalidoException;
+import br.edu.ufape.poo.UfapeMarket.negocio.excecoes.UsuarioEmailJaCadastradoException;
+import br.edu.ufape.poo.UfapeMarket.negocio.excecoes.UsuarioNomeObrigatorioException;
+import br.edu.ufape.poo.UfapeMarket.negocio.excecoes.VendaProdutoObrigatorioException;
+
+public interface InterfaceFachada {
+
+    Usuario salvarUsuario(Usuario usuario)
+            throws UsuarioNomeObrigatorioException,
+                   UsuarioEmailInvalidoException,
+                   UsuarioEmailJaCadastradoException;
+
+    List<Usuario> listarUsuarios();
+
+    Usuario localizarUsuarioEmail(String email);
+
+    Categoria salvarCategoria(Categoria categoria)
+            throws CategoriaDuplicadaException;
+
+    List<Categoria> listarCategorias();
+
+    Chat salvarChat(Chat chat);
+
+    List<Chat> listarChats();
+
+    Mensagem enviarMensagem(Mensagem mensagem)
+            throws MensagemVaziaException;
+
+    Avaliacao avaliarProduto(Avaliacao avaliacao);
+
+    void favoritarProduto(Usuario usuario, Produto produto);
+
+    void escolherCategoria(Produto produto, Categoria categoria)
+            throws ProdutoCategoriaObrigatoriaException;
+
+    void fazerVenda(Venda venda, Produto produto, int quantidade)
+            throws VendaProdutoObrigatorioException,
+                   ProdutoQuantidadeInvalidaException,
+                   ProdutoEstoqueInsuficienteException;
+}
